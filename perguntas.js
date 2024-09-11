6295e8dcfed2cfe728924d8f1342c0c152371d83
felipe557744 <141252103+felipe557744@users.noreply.github.com>
2024-09-11 15:51:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'quantos segundos se tem em 1 ano?',
+    'Um ano tem 31.536.000 segundos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual o melhor livro que você já leu?',
+    '"É assim que acaba" de Colleen hoover'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que vem após a morte?',
+    'O que vem após a morte é um mistério. Diferentes culturas e religiões oferecem explicações variadas, como a ideia de vida após a morte, reencarnação ou um estado de paz. A ciência, por sua vez, foca na cessação das funções biológicas. No fim, a resposta depende das crenças pessoais e filosóficas de cada um.'
+)
